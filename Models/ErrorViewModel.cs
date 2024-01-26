402d2ff77187e0e966d7c6eeff862e09b590588e
Mina Samir <minasamir1411@gmail.com>
2024-01-26 22:52:17 +0200

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Streams_Technology_Task.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
